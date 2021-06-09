5893aee35637b1f51aff6df4968b55fa8a6b415a
Mejar Kumar <Mejarkumar2003@gmail.com>
2021-06-09 19:56:08 +0530

    Adding of salting and hashing in auth

--- app.js
+++ app.js
@@ -1,35 +1,75 @@
 require('dotenv').config();
 
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const passport =require("passport");
+const session=require("express-session");
+const passportLocalMongoose=require("passport-local-mongoose")
+const GoogleStrategy=require("passport-google-oauth20").Strategy;
+const findOrCreate= require("mongoose-findorcreate");
 // const encrypt = require("mongoose-encryption");
 // const md5 =require("md5");
-const bcrypt= require("bcrypt")
+const bcrypt= require("bcrypt");
+const _ = require('lodash');
 const saltRounds =10;
+
+const petitions=[];
 const app = express();
 app.use(express.static("public"));
 app.set("view engine",'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+app.use(session({
+    secret:"our little secret",
+    resave:false,
+    saveUninitialized:false
+}))
 // connect to mongoose with mmongodb 
+// mongoose.connect('mongodb://localhost:27017/PetitionListDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// const petitionSchema = new mongoose.Schema({
+//     Title: String,
+//     TagedAuthority : String,
+//     Descrition: String
+// })
+
+// const Petition = mongoose.model("Petition", petitionSchema);
+
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
    email: String,
    password: String
 });
-
-// userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:['password']});
+userSchema.plugin(findOrCreate);
 
 
 const User = mongoose.model("User",userSchema);
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/petition",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+// userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:['password']});
+
+
+
 app.get('/',(req,res)=>{
     res.render("home",{})
 })
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
 app.get('/login',(req,res)=>{
     res.render("login",{})
 })
@@ -37,10 +77,33 @@ app.get('/register',(req,res)=>{
     res.render("register",{})
 })
 app.get('/petition',(req,res)=>{
-    res.render("petition",{})
+   res.render("petition")
+
 })
 app.get('/petitionlist',(req,res)=>{
-    res.render("petitionlist",{})
+res.render("petitionlist",{
+    petitions:petitions
+});
+// console.log(petitions);
+
+})
+
+
+app.get("/post/:postTitle",(req,res)=>{
+   const requestedPetition= _.lowerCase(req.params.postTitle);
+petitions.forEach((post)=>{
+    const storedPetition = _.lowerCase(post.Title);
+
+if( requestedPetition==storedPetition){
+res.render("post",{
+   Title : post.Title,
+   TagedAuthority : post.TagedAuthority,
+   Description: post.Description
+})
+}
+
+})
+
 })
 
 
@@ -84,8 +147,20 @@ app.post("/login",(req,res)=>{
     })
 })
 
+app.post("/petition",(req,res)=>{
+  const petition={
+        Title:req.body.Title,
+        TagedAuthority:req.body.TagedAuthority,
+        Description:req.body.Description
+  }
+  petitions.push(petition);
 
+res.redirect("/petitionlist");
+})
 
+app.post("petitionlist",(req,res)=>{
+    res.render("petitionlist")
+})
 app.listen(3000, (req,res)=>{
     console.log("the surver is running on the  3000 port.")
 })
---
+++ public/js/post.js
@@ -0,0 +1,4 @@
+const shareButton = document.querySelector('#shareButton');
+const url = window.document.location.href;
+
+
